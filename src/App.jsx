8f702a92bfa7c38f106beacf867825a9edabf685
Siddhesh <siddheshghag150@gmail.com>
2025-11-17 14:47:36 +0530

    added coments for pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='min-h-screen w-screen flex justify-center'>
+    <div className='min-h-screen w-screen flex justify-center flex-col items-center'>
       <Header/>
+      {/* make home page component and mount here */}
+      {/* make getInvoled page component and mount here */}
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowDown } from 'react-icons/io'
 
 const Header = () => {
   return (
-    <div className='flex w-7xl  items-center justify-between h-30'>
+    <div className='flex w-7xl  items-center justify-between h-20'>
         <a href="#home">
             <img src="logo.svg" alt="logo" />
         </a>
